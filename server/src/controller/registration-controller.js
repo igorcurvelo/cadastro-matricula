1eccbbe0b92f9d06009643472da01234af5e2456
Igor Curvelo <igor.curvelo@redspark.io>
2019-01-28 10:40:45 -0200

    finalizando crud

--- server/src/controller/registration-controller.js
+++ server/src/controller/registration-controller.js
@@ -4,35 +4,43 @@ const route = require('express').Router();
 
 route.get('/registration', (req, res, next) => {
     new StudentDao(req.connection)
-    .findAll()
-    .then(students => res.json(students))
-    .catch(next)
+        .findAll()
+        .then(students => res.json(students))
+        .catch(next)
 });
 
 route.get('/registration/:id', (req, res, next) => {
     const id = req.params.id;
     new StudentDao(req.connection)
-    .findOne(id)
-    .then(student => res.json(student))
-    .catch(next)
+        .findOne(id)
+        .then(student => res.json(student))
+        .catch(next)
 })
 
 route.post('/registration', (req, res, next) => {
     var student = req.body
     new StudentDao(req.connection)
-    .save(student)
-    .then(insertId => {
-        student.id = insertId
-        res.json(student)
-    }).catch(next)
+        .save(student)
+        .then(insertId => {
+            student.id = insertId
+            res.json(student)
+        }).catch(next)
 });
 
 route.put('/registrationa/:id', (req, res, next) => {
+    const id = req.params.id
+    new StudentDao(req.connection)
+        .update(id, req.body)
+        .then(student => res.json(student))
+        .catch(next)
+})
+
+route.delete('/registrationa/:id', (req, res, next) => {
     const id = req.params.id;
     new StudentDao(req.connection)
-    .update(id, req.body)
-    .then(student => res.json(student))
-    .catch(next)
+        .delete(id)
+        .then(student => res.json(student))
+        .catch(next)
 })
 
 module.exports = route;
--- server/src/dao/student-dao.js
+++ server/src/dao/student-dao.js
@@ -6,7 +6,7 @@ class StudentDao {
 
     findAll() {
         return new Promise((resolve, reject) =>
-            this._connection.query('SELECT * FROM student', (err, students) => {
+            this._connection.query('SELECT * FROM student WHERE active = 1', (err, students) => {
                 if(err) return reject(err);
                 return resolve(students);
             })
@@ -24,25 +24,32 @@ class StudentDao {
 
     }
 
-    save({name, code}) {
+    delete(studentId) {
         return new Promise((resolve, reject) =>
-            this._connection.query('INSERT INTO student(name, code) values (?, ?)', [ name, code ],
-             (err, results, fields) => {
+            this._connection.query('UPDATE student SET active = 0 WHERE id = ?', [studentId], (err, results, fields) => {
                 if(err) return reject(err);
-                return resolve(results.insertId);
+                return resolve(studentId);
             })
         )
     }
 
     update(studentId, {id, name, code}) {
-        return new Promise((resolve, reject) => 
-        this._connection.query('UPDATE student SET name = ?, code = ? WHERE id = ?')
-        [name, code, studentId], (err, results, fields) => {
-            if(err) return reject(err);
-                return resolve(results[0]);
-        })
+        return new Promise((resolve, reject) =>
+            this._connection.query('UPDATE student SET name = ?, code = ? WHERE id = ?', [name, code, studentId], (err, results, fields) => {
+                if(err) return reject(err);
+                return resolve({id, name, code});
+            })
+        )
     }
 
+    save({name, code}) {
+        return new Promise((resolve, reject) =>
+            this._connection.query('INSERT INTO student(name, code) values (?, ?)', [ name, code ], (err, results, fields) => {
+                if(err) return reject(err);
+                return resolve(results.insertId);
+            })
+        )
+    }
 }
 
 module.exports = StudentDao;
